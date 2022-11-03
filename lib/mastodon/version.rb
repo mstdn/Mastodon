fb6e16b37735828e902aec93f420a613dcf0b955
jwbjnwolf <59905825+jwbjnwolf@users.noreply.github.com>
2022-11-03 05:25:52 +0000

    Update version.rb
    change repo to jwbjnwolf/Mastodon

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mstdn/Mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'jwbjnwolf/Mastodon')
     end
 
     def source_base_url
